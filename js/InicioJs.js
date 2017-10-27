f7984ba651e081f2bfa5ba15c9ddc08ec43a2be1
Cristian <cristian.ramirez019@gmail.com>
2017-10-27 07:15:14 -0500

    Revert "Merge branch 'master' of https://github.com/rmontesl/proyecto-final"
    This reverts commit 5bb2130306d956421f3610092eb8b920cdb44f2c, reversing
    changes made to 06f0c7c2edacfbaf328f7816d0b4b7c29bd4ac5b.

--- js/InicioJs.js
+++ js/InicioJs.js
@@ -1,5 +1,5 @@
 $(document).on("ready",function(){
-	$('#minMaxExample').datepicker({
+	$('#FechayHora').datepicker({
     language: 'es',
      minDate: new Date(),
      timepicker:"true",
@@ -17,30 +17,3 @@ $(document).on("change","#motel",function(){
     else if(idMotel =="Carpe")
         $("#Habitacion").html(habitacionesCarpe);
 });
-
-
-
-$(document).on("click","#enviarForm",function(){
-      var parametros = {
-      "motel" : $("#motel").val(),
-      "FechayHora" : $("#FechayHora").val(),
-      "decoracion" : $("#decoracion").val(),
-      "mail" : $("#mail").val(),
-      "checkbox" : $("#checkbox").val()
-      };
-                
-
-            $.ajax({
-                url: "reserva.php",
-                type: "POST",
-                data: parametros,
-                success: function(data){
-                    $("#formulario").html('');
-                    $("#formulario").html("<fieldset><div id='success_page'><h2 style='color: #541d42 !important;'>¡Gracias por Utilizar Kama!</h2><p style='color: #541d42 !important;'>De 30 a 40 minutos nos estaremos contactando contigo. Espera la confirmación de tu reserva y enkámate.</p></div></fieldset>");
-                    
-                },
-                error: function (xmlHttpRequest, textStatus, errorThrown) {
-                    MensajeError(textStatus);
-                }
-            });
-})
