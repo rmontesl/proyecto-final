3a5ac5fd6200bd2796dee0145592504c0fd09a3a
Cristian <cristian.ramirez019@gmail.com>
2017-10-27 04:49:17 -0500

    Se agrego nuevo archivo js
    Se agrego archivo InicioJs para separar el codigo javascript del html

---
+++ js/InicioJs.js
@@ -0,0 +1,19 @@
+$(document).on("ready",function(){
+	$('#minMaxExample').datepicker({
+    language: 'es',
+     minDate: new Date(),
+     timepicker:"true",
+     position:'right top'
+	});
+});
+
+$(document).on("change","#motel",function(){
+    var habitacionesIbiza = "<option value='0' selected>Selecciona una opción...</option><option value='1'>Sencilla</option><option value='2'>Doble</option>"
+    var habitacionesCarpe = "<option value='0' selected>Selecciona una opción...</option><option value='3'>Habitacion 1</option><option value='4'>Suite</option>"
+    var idMotel = $("#motel option:selected").val();
+
+    if(idMotel == "Ibiza")
+        $("#Habitacion").html(habitacionesIbiza);
+    else if(idMotel =="Carpe")
+        $("#Habitacion").html(habitacionesCarpe);
+});
